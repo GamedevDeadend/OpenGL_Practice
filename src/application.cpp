a356b7982de4ce63b378ea988101c4a6c040f1fc
GamedevDeadend <tanmay10087@gmail.com>
2024-03-15 20:57:42 +0530

    Added GLEW VertexBuffer & DrawCalls Intro

--- src/application.cpp
+++ src/application.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Rendering..." << std::endl;
 
     GLFWwindow* window;
 
@@ -22,6 +23,32 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glewInit();
+
+    if (glewInit() == GLEW_OK)
+    {
+        std::cout << "Init Success" << std::endl;
+        std::cout << glGetString(GL_VERSION)  << std::endl;
+    }
+
+    unsigned int buffer = 7;
+
+    //for blocking buffer
+    glGenBuffers(1, &buffer);
+
+    // for selecting buffer and use it as array
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    float positions[] =
+    {
+        (-0.5f,-0.5f),
+        (0.0f,0.5f),
+        (0.5f,-.5f)
+    };
+
+    //Specify size and give data
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     
     while (!glfwWindowShouldClose(window))
@@ -30,11 +57,18 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*Draw Triangle using Legacy OpenGL*/
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f,-0.5f);
-        glVertex2f(0.0f,0.5f);
-        glVertex2f(0.5f,-.5f);
-        glEnd();
+
+        //Draw Call to draw triangle using assigned buffer
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        //Another Way for DrawCall that we will use later on
+        //glDrawElements(GL_TRIANGLES, 6, GL_STATIC_DRAW);
+
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f( -0.5f,-0.5f);
+        //glVertex2f( 0.0f,0.5f);
+        //glVertex2f( 0.5f,-.5f);
+        //glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
